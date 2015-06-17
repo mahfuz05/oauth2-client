6b4bbeeefe7371e26d67af95052869792f1e3c09
vagrant <vagrant@homestead>
2015-06-17 15:13:09 +0000

    first commit

---
+++ src/Mahfuz05/LaravelOauth2/LaravelOauth2ServiceProvider.php
@@ -0,0 +1,52 @@
+<?php namespace Mahfuz05\LaravelOauth2;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelOauth2ServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('mahfuz05/laravel-oauth2');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+        // Register 'oauth2'
+        $this->app['oauth2'] = $this->app->share(function($app)
+        {
+            $config = $app['config'];
+            // create oAuth instance
+            $oauth2 = new LaravelOauth2($config->get('config.client.id'), $config->get('config.client.secret'));
+            // return oAuth instance
+            return $oauth2;
+        });
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
---
+++ src/Mahfuz05/config/config.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package Laravel OAuth2
+ * @author  Mahfuzur Rahman
+ * Date:    17.06.15
+ */
+
+return array(
+
+    'client'		=> array(
+
+        /*
+         * Client-ID of your Application
+         */
+        'id'		=> '',
+
+        /*
+         * Client-Secret of your Application
+         */
+        'secret'	=> ''
+
+    ),
+
+    /*
+     * Scopes for your Application (comma separated)
+     */
+    'scopes'		=> ''
+
+);
